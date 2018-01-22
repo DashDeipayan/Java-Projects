c4dd88624386a95547074deeaac7919f137a2751
Dash Deipayan <deipayan.dash8@gmail.com>
2018-01-22 19:08:34 +0530

    Incomplete Partially

---
+++ IdeaProjects/Code#1/src/Code_1.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+
+public class Code_1 {
+    public static int sumOfArray(int[] a){
+        int s=0;
+        for(int i=0;i<a.length;i++){
+            s=s+a[i];
+
+        }
+        return s;
+    }
+    public static void main(String[] args){
+
+    }
+
+}
